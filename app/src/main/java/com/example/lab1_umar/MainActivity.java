150db86523505cfb9ba7f04a8c8ba4ace5e832b2
UmarYusuf <umaryusuf073@gmail.com>
2023-11-05 14:58:13 +0800

    First commit

---
+++ app/src/main/java/com/example/lab1_umar/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.lab1_umar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvMessage;
+    Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvMessage = (TextView) findViewById(R.id.textView);
+        submitButton = (Button) findViewById(R.id.button);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void displayText(View view){
+
+        tvMessage.setText("Welcome!");
+    }
+
+}
